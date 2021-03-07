01b8cd3dcc475b92b2bbc2823ff4eebf7ab9ffd1
Thiago Moreira <ttrmoreira@gmail.com>
2021-03-07 14:48:31 -0300

    First commit

---
+++ 5_1/script.js
@@ -0,0 +1,21 @@
+document.querySelector("#header-container h1").style.backgroundColor = "green";
+document.querySelector("#header-container h1").style.color = "white";
+
+document.querySelectorAll(".emergency-tasks div")[0].style.backgroundColor = "orange";
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[0].style.color = "white";
+
+document.querySelectorAll(".emergency-tasks div")[1].style.backgroundColor = "orange";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.color = "white";
+
+document.querySelectorAll(".no-emergency-tasks div")[0].style.backgroundColor = "yellow";
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.color = "white";
+
+document.querySelectorAll(".no-emergency-tasks div")[1].style.backgroundColor = "yellow";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.color = "white";
+
+
+document.querySelector("#footer-container div").style.backgroundColor = "green";
